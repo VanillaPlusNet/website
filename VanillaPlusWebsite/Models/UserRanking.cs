e147049776abd4f1e0d91d286ae2094af2571671
GoldenEdit <sammyhmatthews@gmail.com>
2024-04-30 08:55:53 +0100

    statiq

---
+++ VanillaPlusWebsite/Models/UserRanking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaPlusWebsite.Models
+{
+    public class UserRanking
+    {
+        public string? UUID { get; set; }
+
+        public int Points { get; set; }
+
+        public int Rank { get; set; }
+    }
+}
