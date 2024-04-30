552c1d9ec4653ed288a255803301b1f738e1db2d
GoldenEdit <sammyhmatthews@gmail.com>
2024-04-30 10:17:56 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace VanillaPlusWebsite
             var currentDirectory = Directory.GetCurrentDirectory();
             var nodeDirectory = Path.Combine(currentDirectory, ".", "node");
             var inputDirectory = Path.Combine(currentDirectory, "input");
+            var outputDirectory = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
 
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
 
@@ -29,7 +30,7 @@ namespace VanillaPlusWebsite
                     ProcessTiming.AfterExecution,
                     _ => new ProcessLauncher("npx", "tailwind", "build",
                         $"-i {Path.Combine(inputDirectory,"assets", "css", "_site.css")}",
-                        $"-o {Path.Combine(currentDirectory, "output", "styles.css")}")
+                        $"-o {Path.Combine(outputDirectory, "styles.css")}")
                     {
                         LogErrors = false,
                         WorkingDirectory = nodeDirectory
